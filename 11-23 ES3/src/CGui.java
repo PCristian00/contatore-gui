284dfd5a227a2aa069b826b3d924cab922a93c57
PCristian00 <95343580+PCristian00@users.noreply.github.com>
2021-12-01 19:24:26 +0100

    Update CGui.java

--- 11-23 ES3/src/CGui.java
+++ 11-23 ES3/src/CGui.java
@@ -10,21 +10,22 @@ public class CGui extends JFrame {
 	Contatore c = new Contatore();
 
 	public CGui() {
+		//impostazioni finestra
 		setTitle("Contatore");
 		setSize(400, 800);
-
+		//creazione pulsanti
 		JButton mst = new JButton("MOSTRA");
 		JButton inc = new JButton("INCREMENTA");
 		JButton dec = new JButton("DECREMENTA");
 		JButton azz = new JButton("AZZERA");
-
+		//azioni dei pulsanti
 		mst.addActionListener(new Ascolta());
 		inc.addActionListener(new Ascolta());
 		dec.addActionListener(new Ascolta());
 		azz.addActionListener(new Ascolta());
-
+		//creazione pannello
 		JPanel panel = new JPanel();
-
+		//aggiunta dei pulsanti creati al pannello
 		panel.add(mst);
 		panel.add(inc);
 		panel.add(dec);
